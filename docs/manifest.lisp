afd9a796184811082111e8fcb35c854633a4b7ab
Alessandro Serra <gas2serra@gmail.com>
2015-12-27 00:30:32 +0100

    add docs using codex

---
+++ docs/manifest.lisp
@@ -0,0 +1,9 @@
+(:docstring-markup-format :scriba
+ :systems (:cl-turtle)
+ :documents ((:title "Common Lisp Turtle Graphics"
+              :authors ("Alessandro Serrs (gas2serra@gmail.com")
+              :output-format (:type :multi-html
+                              :template :gamma)
+              :sources ("overview.scr"
+                        "tutorial.scr"
+			"internals.scr"))))
